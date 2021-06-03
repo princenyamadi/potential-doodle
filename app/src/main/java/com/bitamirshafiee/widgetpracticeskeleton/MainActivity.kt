a2cb6fe8a4a2bd6524b2254cd412e3a033b2f179
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-06-03 16:08:09 +0000

    implementing a seekbar in kotlin

--- app/src/main/java/com/bitamirshafiee/widgetpracticeskeleton/MainActivity.kt
+++ app/src/main/java/com/bitamirshafiee/widgetpracticeskeleton/MainActivity.kt
@@ -2,10 +2,27 @@ package com.bitamirshafiee.widgetpracticeskeleton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        age_text_view.text = String.format(resources.getString(R.string.str_age), 0)
+        seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                age_text_view.text = String.format(resources.getString(R.string.str_age), progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
     }
 }
